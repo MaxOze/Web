41ffc6b9faeab3819de28efd9c36b19f536df41a
maks <2002ozerov@mail.ru>
2022-10-28 22:29:36 +0500

    Add entity for request user input

---
+++ src/main/java/com/example/web/util/Input.java
@@ -0,0 +1,15 @@
+package com.example.web.util;
+
+public class Input {
+    public String input;
+
+    public Input() { }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
